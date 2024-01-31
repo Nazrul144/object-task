ab341464b9b4fefd1834c30601d7e51da343a062
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-01-31 17:43:42 +0600

    third commit

---
+++ object-practice3.js
@@ -0,0 +1,13 @@
+//Create an object named student with some property and values then illustrate the physics marks as output.
+
+let student = {
+    name:'Nazrul Islam',
+    id:211002144,
+    physics:{
+        subject:'HSC Physics',
+        author: 'hahjahan Tapan',
+        marks:30
+    }
+}
+
+console.log(student.physics.marks); // Here I have used dot notation to access the mark value
