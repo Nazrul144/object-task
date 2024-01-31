e98062f47fbb92846f8c331ef06d4bcb03f660a2
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-01-31 17:37:06 +0600

    second commit

---
+++ object-practice2.js
@@ -0,0 +1,11 @@
+//Create an object and add a property named passenger capacity with value five.
+
+let car = {
+    make:'Toyota',
+    model:'Corolla',
+    year:2020
+}
+
+car['passenger capacity'] = ['bus:50','Air Plane:150','Ship:3000','Rickshaw:2','Bike:2'];
+console.log(car);
+
