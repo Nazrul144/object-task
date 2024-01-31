88a95c5102a0f80a726f2f618e8fb3779f5f05e6
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-01-31 17:20:37 +0600

    first commit

---
+++ object-practice1.js
@@ -0,0 +1,8 @@
+let colors = {
+    red:'#0000F',
+    green:'#000CS',
+    blue:'#000SS',
+    'golden rod':'#daa520'
+}
+
+console.log(colors['golden rod']); //Here I have used square bracket to access the object's value.
