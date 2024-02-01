ad0f6708882684f4c7c39cad09f32628e1431d5d
Nazrul144 <nazrulislam.cse28@gmail.com>
2024-02-01 13:30:45 +0600

    fourth commit

---
+++ string-practice4.js
@@ -0,0 +1,13 @@
+// If a given string has either x, replace x by y. if the given string has X, replace it by Y.
+
+let stringX = 'X';
+
+let newStringX = stringX.replace('x','X');
+
+console.log(newStringX);
+
+let stringY = 'y';
+
+let newStringY = stringY.replace('x','y');
+
+console.log(newStringY);
